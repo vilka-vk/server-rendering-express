183f7c32f274680025e643f6481da79f8130f1c5
Violetta Morozova <v@cifronomika.ru>
2018-07-24 00:20:32 +0300

    Added array of objects

---
+++ src/constants/Products.js
@@ -0,0 +1,18 @@
+export default Products = [
+  {
+    id: 111,
+    title: 'Торт зефирный',
+    price: 245,
+    imageUrl: 'http://s017.radikal.ru/i438/1302/f9/da6af048d722.jpg'
+  },{
+    id: 112,
+    title: 'Торт муссовый',
+    price: 560,
+    imageUrl: 'https://avatars.mds.yandex.net/get-pdb/966350/a88f5840-8aef-423b-9c62-681254003b59/orig'
+  },{
+    id: 113,
+    title: 'Торт клубничный йогуртовый',
+    price: 430,
+    imageUrl: 'http://vkuso.ru/img/jogurtovyj-tort-zhele-s-klubnikoj.jpg'
+  }
+];
